6e6fc269e337f91b2c93d2ba00738e121ae75a56
mervekocabas <merve.kocabas@outlook.com>
2025-06-26 14:57:48 +0200

    dwpose+bedlam kpts vids

--- bedlam_100_face_extract.py
+++ bedlam_100_face_extract.py
@@ -5,7 +5,8 @@ import os
 
 def run_inference(idx, file_name):
     #video_dir = Path("./results/vace-14B-v2prompt-face-body-videos")
-    video_dir = Path("./results/bedlam_src_vids_16fps")
+    video_dir = Path("./results/bedlam_100_videos_face_hand_vids_dwpose_30fps_one_directory")
+    
     src_video = video_dir / file_name
 
     if not src_video.exists():
