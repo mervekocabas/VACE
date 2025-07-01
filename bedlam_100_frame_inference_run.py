c4c9860e60840975e35a55936a515b4cfb9bbea5
mervekocabas <merve.kocabas@outlook.com>
2025-07-01 14:55:47 +0200

    frame by frame

--- bedlam_100_frame_inference_run.py
+++ bedlam_100_frame_inference_run.py
@@ -132,11 +132,11 @@ def run_inference(idx, video_name, prompt):
             temp_dir.rmdir()
 
 if __name__ == "__main__":
-    csv_path = "./vace_bedlam_100_dataset/final_metadata_1.csv"
-    df = pd.read_csv(csv_path, delimiter=';')
+    #csv_path = "./vace_bedlam_100_dataset/final_metadata_1.csv"
+    #df = pd.read_csv(csv_path, delimiter=';')
 
-    for idx, row in df.iterrows():
-        run_inference(idx, row["file_name"], row["text"])
+    #for idx, row in df.iterrows():
+    #    run_inference(idx, row["file_name"], row["text"])
     
     # After all inferences are done, run post-processing
     concatenate_chunks_to_sequence_output()
