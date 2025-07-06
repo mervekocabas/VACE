37c853c4237c3e654029adf05beb2563cd2aae73
mervekocabas <merve.kocabas@outlook.com>
2025-07-06 11:35:41 +0200

    diffsynth

--- diffsynthinference.py
+++ diffsynthinference.py
@@ -20,11 +20,12 @@ pipe = WanVideoPipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
     device="cuda",
     model_configs=[
-        ModelConfig(model_id="models/VACE-Wan2.1-14B", origin_file_pattern="diffusion_pytorch_model*.safetensors", offload_device="cpu"),
-        ModelConfig(model_id="models/VACE-Wan2.1-14B", origin_file_pattern="models_t5_umt5-xxl-enc-bf16.pth", offload_device="cpu"),
-        ModelConfig(model_id="models/VACE-Wan2.1-14B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="diffusion_pytorch_model*.safetensors", offload_device="cpu"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="models_t5_umt5-xxl-enc-bf16.pth", offload_device="cpu"),
+        ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu"),
     ],
-    redirect_common_files=False  # Optional, usually not needed
+    cache_dir="models/VACE-Wan2.1-14B", 
+    local_files_only=True,  
 )
 #pipe = WanVideoPipeline.from_pretrained(
 #    "models/VACE-Wan2.1-14B",
