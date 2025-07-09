3c2e3e0846d6bde7b664a6de3e669700130a1519
mervekocabas <merve.kocabas@outlook.com>
2025-07-09 09:26:20 +0200

    diffsynth

--- diffsynthinference.py
+++ diffsynthinference.py
@@ -33,13 +33,13 @@ pipe = WanVideoPipeline.from_pretrained(
 pipe = WanVideoPipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
     device="cuda",
+    redirect_common_files=False,
     model_configs=[
         ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="diffusion_pytorch_model*.safetensors", offload_device="cpu"),
         ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="models_t5_umt5-xxl-enc-bf16.pth", offload_device="cpu"),
         ModelConfig(model_id="Wan-AI/Wan2.1-VACE-14B", origin_file_pattern="Wan2.1_VAE.pth", offload_device="cpu"),
     ],
     skip_download = True,
-    
 )
 
 pipe.enable_vram_management()
@@ -398,6 +398,7 @@ def run_inference(idx: int, video_name: str, prompt: str):
             seed=1, tiled=True,
             height = height_frame,
             width = width_frame,
+            negative_prompt='色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走',
         )
         
         import ipdb; ipdb.set_trace()
