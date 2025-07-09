8334d1cb0e99dd0f1f4ca0842333e9eb0c110c01
mervekocabas <merve.kocabas@outlook.com>
2025-07-09 14:13:06 +0200

    diffsynth

--- diffsynthinference.py
+++ diffsynthinference.py
@@ -395,7 +395,7 @@ def run_inference(idx: int, video_name: str, prompt: str):
             prompt=prompt,
             vace_video=src_convid,
             #vace_video_mask = mask_convid,
-            seed=2025, tiled=False,
+            seed=2025, tiled=True,
             height = height_frame,
             width = width_frame,
             sigma_shift = 16.0,
