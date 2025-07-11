0c1c7acf5a1752c55f6b8d062e3bf0edc8add5ab
mervekocabas <merve.kocabas@outlook.com>
2025-07-12 00:48:27 +0200

    keypoint test

--- bedlam_100_face_extract.py
+++ bedlam_100_face_extract.py
@@ -28,7 +28,7 @@ def run_inference(idx, file_name):
     subprocess.run(cmd, env=env)
 
 if __name__ == "__main__":
-    csv_path = "./vace_bedlam_100_dataset/parallel_2.csv"
+    csv_path = "./vace_bedlam_100_dataset/final_metadata.csv"
     df = pd.read_csv(csv_path, delimiter=';')
 
     for idx, row in df.iterrows():
