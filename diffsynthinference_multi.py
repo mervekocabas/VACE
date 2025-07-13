6136cb126e0b3912ecca1b25f9b9f3ed1e0f33ee
mervekocabas <merve.kocabas@outlook.com>
2025-07-13 13:23:46 +0200

    diffsynth inference

--- diffsynthinference_multi.py
+++ diffsynthinference_multi.py
@@ -392,8 +392,9 @@ def run_inference(idx: int, video_name: str, prompt: str):
         save_video_frames(video, output_dir)       
        
 if __name__ == "__main__":
-    csv_path = "./vace_bedlam_100_dataset/parallel_2.csv"
-    df = pd.read_csv(csv_path, delimiter=';')
+    #csv_path = "./vace_bedlam_100_dataset/parallel_2.csv"
+    #df = pd.read_csv(csv_path, delimiter=';')
 
-    for idx, row in df.iterrows():
-        run_inference(idx, row["file_name"], row["text"])
+    #for idx, row in df.iterrows():
+    #    run_inference(idx, row["file_name"], row["text"])
+    concatenate_chunks_to_sequence_output()
